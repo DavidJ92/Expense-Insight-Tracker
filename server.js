3a5ef7af1bc260a56ed0ac43c8986fc316f43ef9
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2023-12-15 23:32:38 -0600

    added path and routes to server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const hbs = exphbs.create({ helpers });
 
@@ -14,9 +17,14 @@ app.set('view engine', 'handlebars');
 
 // Set up middleware
 app.use(cookieParser());
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true, store: new SequelizeStore({
+    db: sequelize}) }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//use routes
+app.use(routes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
