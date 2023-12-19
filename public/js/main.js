bfbe6fb7951027215029ee6af8e1f104020376c2
Xue <xuefeng.yangg@gmail.com>
2023-12-18 19:49:35 -0600

    made changes in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,15 @@
-// Sample expense data (replace this with your logic to handle expenses)
-let expenses = [] // Array to store expenses
+// Get references to the HTML elements
+const expenseForm = document.getElementById('expenseForm');
+const expenseList = document.getElementById('expenseList');
+
+// Array to store expenses
+let expenses = [];
 
 // Function to render expenses
 function renderExpenses() {
-  const expenseList = document.getElementById('expenseList');
-  expenseList.innerHTML = ''; // Clear previous content
+ expenseList.innerHTML = ''; // Clear previous content
   
-  expenses.forEach(expense => {
+ expenses.forEach(expense => {
     const expenseDiv = document.createElement('div');
     expenseDiv.classList.add('expense-item');
     expenseDiv.innerHTML = `
@@ -17,27 +20,25 @@ function renderExpenses() {
       <p>Comment: ${expense.comment}</p>
     `;
     expenseList.appendChild(expenseDiv);
-  });
+ });
 }
 
 // Function to handle form submission
-document.getElementById('expenseForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
+expenseForm.addEventListener('submit', function(event) {
+ event.preventDefault(); // Prevent form submission
   
-  const title = document.getElementById('expenseTitle').value;
-  const date = document.getElementById('expenseDate').value;
-  const amount = document.getElementById('expenseAmount').value;
-  const category = document.getElementById('expenseCategory').value;
-  const comment = document.getElementById('expenseComment').value;
+ const title = document.getElementById('expenseTitle').value;
+ const date = document.getElementById('expenseDate').value;
+ const amount = document.getElementById('expenseAmount').value;
+ const category = document.getElementById('expenseCategory').value;
+ const comment = document.getElementById('expenseComment').value;
   
-  // Add the new expense to the expenses array
-  expenses.push({ title, date, amount, category, comment });
+ // Add the new expense to the expenses array
+ expenses.push({ title, date, amount, category, comment });
   
-  // Render expenses
-  renderExpenses();
+ // Render expenses
+ renderExpenses();
   
-  // Clear form fields after submission
-  document.getElementById('expenseForm').reset();
+ // Clear form fields after submission
+ expenseForm.reset();
 });
-
-  
