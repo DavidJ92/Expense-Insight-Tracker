84c3df81262d614aaed38aeddd07218d9f9baa76
Xue <xuefeng.yangg@gmail.com>
2023-12-18 19:24:24 -0600

    made a touch up in home-router.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,69 +2,71 @@ const router = require('express').Router();
 const { Spending } = require('../models');
 const withAuth = require('../utils/auth');
 
-//if user is logged in, show line chart for the year
+// GET homepage with line chart
 router.get('/', withAuth, async (req, res) => {
-  const spendingHistory = await Spending.findAll();
-  res.render('index', { spendingHistory });
+ const spendingHistory = await Spending.findAll();
+ res.render('index', { spendingHistory });
 });
 
+// POST new spending entry
 router.post('/spending', withAuth, async (req, res) => {
-  try {
-      const newSpending = await Spending.create(req.body);
-      res.json(newSpending);
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-  }
+ try {
+    const newSpending = await Spending.create(req.body);
+    res.json(newSpending);
+ } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+ }
 });
 
+// GET all spending entries for the year
 router.get('/spending', withAuth, async (req, res) => {
-  try {
-      const spendingData = await Spending.findAll();
-      res.json(spendingData);
-    
-      res.render('main', {
-        expenseChart,
-        loggedIn: req.session.loggedIn,
-      });
+ try {
+    const spendingData = await Spending.findAll();
+    res.json(spendingData);
 
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-  }
+    res.render('main', {
+      expenseChart,
+      loggedIn: req.session.loggedIn,
+    });
+
+ } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+ }
 });
 
-//login route
+// GET login route
 router.get('/login', (req, res) => {
-  //if user is logged in, redirects to homepage
-  if (req.session.loggedIn) {
+ // if user is logged in, redirects to homepage
+ if (req.session.loggedIn) {
     res.redirect('/');
     return;
-  }
-  //else render login.handlebars
-  res.render('login');
+ }
+ // else render login.handlebars
+ res.render('login');
 });
 
-//sign up route
+// GET sign up route
 router.get('/signup', (req, res) => {
-  //if user is logged in, redirects to homepage
-  if (req.session.loggedIn) {
+ // if user is logged in, redirects to homepage
+ if (req.session.loggedIn) {
     res.redirect('/');
     return;
-  }
-  //else render signup.handlebars
-  res.render('signup');
+ }
+ // else render signup.handlebars
+ res.render('signup');
 });
 
-//logout route
+// GET logout route
 router.get('/logout', (req, res) => {
-    //if user is logged out, redirects to logout page
-    if (req.session.loggedOut) {
-      res.redirect('/');
-      return;
-    }
-    //else render logout.handlebars
-    res.render('logout');
-  });
+ // if user is logged out, redirects to logout page
+ if (req.session.loggedOut) {
+    res.redirect('/');
+    return;
+ }
+ // else render logout.handlebars
+ res.render('logout');
+});
 
 module.exports = router;
