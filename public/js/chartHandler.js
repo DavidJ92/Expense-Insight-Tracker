601b723c0dd7a5a29e9c41302c75b9457aa8daec
DavidJ92 <141689412+DavidJ92@users.noreply.github.com>
2023-12-20 19:32:16 -0600

    fixing chart location

--- public/js/chartHandler.js
+++ public/js/chartHandler.js
@@ -1,58 +1,60 @@
-// chartHandler.js
-
 // Sample data for monthly expenses (replace this with your logic)
 const monthlyExpenses = {
-  January: 1500,
-  February: 2200,
-  March: 1800,
-  April: 1200,
-  May: 2000,
-  June: 2500,
-  July: 2200,
-  August: 1900,
-  September: 1800,
-  October: 2800,
-  November: 2600,
-  December: 2200
-  // ... Add more months and expenses as needed
-};
-
-
-// Function to render monthly expense chart
-function renderExpenseChart() {
-  const ctx = document.getElementById('myChart');
-
-  // Create Chart instance
-    new Chart(ctx, {
+    January: 1500,
+    February: 2200,
+    March: 1800,
+    April: 1200,
+    May: 2000,
+    June: 2500,
+    July: 2200,
+    August: 1900,
+    September: 1800,
+    October: 2800,
+    November: 2600,
+    December: 2200
+  };
+  
+  // Function to render the monthly expense chart
+  function renderExpenseChart() {
+    const currentPage = window.location.pathname; // Get the current page URL
+  
+    // Check if the current page is the home page
+    if (currentPage === '/home') {
+      const ctx = document.getElementById('myChart');
+  
+      // Create Chart instance
+      new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['January',
-                    'February',
-                    'March' ,
-                    'April' ,
-                    'May' ,
-                    'June' ,
-                    'July' ,
-                    'August' ,
-                    'September' ,
-                    'October' ,
-                    'November' ,
-                    'December' ],
-                        datasets: [{
-                            label: 'Amount Spent',
-                            data: monthlyExpenses,
-                            borderWidth: 1
-                        }]
+          labels: ['January',
+           'February', 
+           'March', 
+           'April', 
+           'May', 
+           'June', 
+           'July', 
+           'August',
+            'September', 
+            'October',
+             'November', 
+             'December'],
+          datasets: [{
+            label: 'Amount Spent',
+            data: Object.values(monthlyExpenses),
+            borderWidth: 1
+          }]
         },
         options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
+          scales: {
+            y: {
+              beginAtZero: true
             }
+          }
         }
-    });
-}
-
-// Call the function to render the chart
-renderExpenseChart();
+      });
+    }
+  }
+  
+  // Call the function to render the chart
+  renderExpenseChart();
+  
