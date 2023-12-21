49516dc752136bc5de967304af8dc9989175580c
Xue <xuefeng.yangg@gmail.com>
2023-12-20 21:53:35 -0600

    updated charthandler.js

--- public/js/chartHandler.js
+++ public/js/chartHandler.js
@@ -1,60 +1,64 @@
-// Sample data for monthly expenses (replace this with your logic)
-const monthlyExpenses = {
-    January: 1500,
-    February: 2200,
-    March: 1800,
-    April: 1200,
-    May: 2000,
-    June: 2500,
-    July: 2200,
-    August: 1900,
-    September: 1800,
-    October: 2800,
-    November: 2600,
-    December: 2200
-  };
-  
-  // Function to render the monthly expense chart
-  function renderExpenseChart() {
-    const currentPage = window.location.pathname; // Get the current page URL
-  
-    // Check if the current page is the home page
-    if (currentPage === '/') {
-      const ctx = document.getElementById('myChart');
-  
-      // Create Chart instance
-      new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['January',
-           'February', 
-           'March', 
-           'April', 
-           'May', 
-           'June', 
-           'July', 
-           'August',
-            'September', 
-            'October',
-             'November', 
-             'December'],
-          datasets: [{
-            label: 'Amount Spent',
-            data: Object.values(monthlyExpenses),
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
+// User's monthly expenses data (replace this with the user's logic)
+let userMonthlyExpenses = {
+  January: 0,
+  February: 0,
+  March: 0,
+  April: 0,
+  May: 0,
+  June: 0,
+  July: 0,
+  August: 0,
+  September: 0,
+  October: 0,
+  November: 0,
+  December: 0
+};
+
+// Function to add monthly expense for the user
+function addMonthlyExpense(month, amount) {
+  userMonthlyExpenses[month] = amount;
+}
+
+// Function to render the monthly expense chart
+function renderExpenseChart() {
+  const currentPage = window.location.pathname; // Get the current page URL
+
+  // Check if the current page is the home page
+  if (currentPage === '/main.html') {
+    const ctx = document.getElementById('myChart');
+
+    // Create Chart instance
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['January',
+         'February', 
+         'March', 
+         'April', 
+         'May', 
+         'June', 
+         'July', 
+         'August',
+          'September', 
+          'October',
+           'November', 
+           'December'],
+        datasets: [{
+          label: 'Amount Spent',
+          data: Object.values(userMonthlyExpenses),
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
           }
         }
-      });
-    }
+      }
+    });
   }
-  
-  // Call the function to render the chart
-  renderExpenseChart();
-  
+}
+
+// Call the function to render the chart
+renderExpenseChart();
