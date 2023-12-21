db83063eccde12d96ef3445b86797fb784468cc7
DavidJ92 <141689412+DavidJ92@users.noreply.github.com>
2023-12-21 01:20:02 -0600

    update server.js for the page to be load in login/singin by default

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-// const cookieParser = require('cookie-parser');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
@@ -19,23 +18,43 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Define a middleware function to redirect users to the login page if not logged in
+const redirectToLogin = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/login'); // Redirect to the login page if not logged in
+  } else {
+    next(); // Continue to the next middleware if user is logged in
+  }
+};
+
+// Apply the redirectToLogin middleware to handle all incoming requests
+app.use((req, res, next) => {
+  if (!req.session.loggedIn && req.url !== '/login' && req.url !== '/signup') {
+    res.redirect('/login'); // Redirect to login if not logged in and not on login/signup pages
+  } else {
+    next();
+  }
+});
+
 // Set up middleware
-// app.use(cookieParser());
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true, store: new SequelizeStore({
-    db: sequelize}) }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize }),
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//use routes
+// Use routes
 app.use(routes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
 const { User } = require("./models");
-sequelize.sync({ force: false }).then( async ()  => {
-  
-  User.findAll().then( async (users) => {
+sequelize.sync({ force: false }).then(async () => {
+  User.findAll().then(async (users) => {
     if (users.length === 0) {
       await seedUsers();
       await seedExpenses();
