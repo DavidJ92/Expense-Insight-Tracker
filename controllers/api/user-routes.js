acd8407ebc6c3ddd1dff7b9a099f49a6477f8526
Xue <xuefeng.yangg@gmail.com>
2023-12-20 19:36:18 -0600

    update

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,7 +49,7 @@ router.post('/', async (req, res) => {
     });
 
     req.session.save(() => {
-      req.session.user_id = dbUserData.id  
+      req.session.user_id = dbUserData.id
       req.session.loggedIn = true;
 
       res.status(200).json(dbUserData);
