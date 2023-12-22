0a1ec0daa11b8e55108acc0433570693840c6bed
Xue <xuefeng.yangg@gmail.com>
2023-12-21 20:17:33 -0600

    added tracking for the chart

--- public/js/chartHandler.js
+++ public/js/chartHandler.js
@@ -1,72 +1,24 @@
-// User's monthly expenses data (replace this with the user's logic)
-let userMonthlyExpenses = {
-  January: 0,
-  February: 0,
-  March: 0,
-  April: 0,
-  May: 0,
-  June: 0,
-  July: 0,
-  August: 0,
-  September: 0,
-  October: 0,
-  November: 0,
-  December: 0
+let data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  datasets: [{
+     label: 'Money Spent per Month',
+     data: [300, 200, 100, 200, 100, 300, 400, 100, 300, 200, 100, 300],
+     borderColor: 'rgba(75, 192, 192, 1)',
+     backgroundColor: 'rgba(75, 192, 192, 0.2)',
+  }]
  };
  
- // Function to add monthly expense for the user
- function addMonthlyExpense(month, amount) {
-  userMonthlyExpenses[month] = amount;
- }
- 
- // Function to render the monthly expense chart
- async function renderExpenseChart() {
-  const response = await fetch("/api/expenses");
-  console.log(response);
- 
-  // Check if the current page is the home page
-  if (currentPage === '/') {
-     const ctx = document.getElementById('myChart').getContext('2d');
- 
-     // Create Chart instance
-     new Chart(ctx, {
-       type: 'line',
-       data: {
-         labels: ['January',
-          'February', 
-          'March', 
-          'April', 
-          'May', 
-          'June', 
-          'July', 
-          'August',
-           'September', 
-           'October',
-            'November', 
-            'December'],
-         datasets: [{
-           label: 'Amount Spent',
-           data: Object.values(userMonthlyExpenses),
-           borderWidth: 1
-         }]
-       },
-       options: {
-         scales: {
-           y: {
-             beginAtZero: true
-           }
-         }
-       }
-     });
+ let options = {
+  scales: {
+     y: {
+       beginAtZero: true
+     }
   }
- }
+ };
  
- // Call the function to render the chart
- renderExpenseChart();
-
-// Function to handle adding a new expense
-
-
-
-
- 
+ let ctx = document.getElementById('myChart').getContext('2d');
+ let chart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: options
+ });
