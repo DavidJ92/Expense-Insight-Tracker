095730d9c0e7ddfa4cd793461a863cac7c88f1b0
Xue <xuefeng.yangg@gmail.com>
2023-12-21 18:08:29 -0600

    added tracking for the chart

--- public/js/addExpense.js
+++ public/js/addExpense.js
@@ -1,69 +1,55 @@
 // addExpense.js
+const addFormHandler = async (event) => {
+  event.preventDefault();
 
-// Function to add monthly expense for the user
-const addExpense = async (expense) => {
-    try {
+  const date = document.querySelector('#date').value.trim();
+  const category = document.querySelector('#categories').value.trim();
+  const amount = document.querySelector('#amount').value.trim();
+  const description = document.querySelector('#description').value.trim();
+
+  if (date && category && amount) {
       const response = await fetch('/api/expenses/add-expense', {
-        method: 'POST',
-        body: JSON.stringify(expense),
-        headers: { 'Content-Type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify({ date, category, amount }),
+          headers: { 'Content-Type': 'application/json' },
       });
-  
+
       if (response.ok) {
-        const addedExpense = await response.json();
-        fetchAndAppendExpense(); // Fetch the updated expense list and append the new expense to the existing list
-        return addedExpense;
+          alert('Expense added!');
+          // Fetch the updated expense list and append the new expense to the existing list
+          fetchAndAppendExpense();
       } else {
-        throw new Error('Failed to add expense. Please try again.');
+          alert('Failed to add expense. Please try again.');
       }
-    } catch (error) {
-      console.error('Error adding expense:', error);
-      throw error;
-    }
-  };
-  
-  // Function to fetch the newly added expense and append it to the list
-  const fetchAndAppendExpense = async () => {
-    try {
+  }
+};
+
+document.querySelector('.add-form').addEventListener('submit', addFormHandler);
+
+// Function to fetch the newly added expense and append it to the list
+const fetchAndAppendExpense = async () => {
+  try {
       const response = await fetch('/api/expenses/latest'); // Endpoint to fetch the latest expense added
       if (response.ok) {
-        const expense = await response.json();
-        appendExpenseToList(expense);
+          const expense = await response.json();
+          appendExpenseToList(expense);
       } else {
-        console.error('Failed to fetch the latest expense');
+          console.error('Failed to fetch the latest expense');
       }
-    } catch (error) {
+  } catch (error) {
       console.error('Error fetching the latest expense:', error);
-    }
-  };
-  
-  // Function to append the expense to the existing list
-  const appendExpenseToList = (expense) => {
-    const listContainer = document.querySelector('.list-details ul');
-  
-    const listItem = document.createElement('li');
-    listItem.textContent = `${expense.date} - ${expense.category} - ${expense.amount} - ${expense.description}`;
-    listContainer.appendChild(listItem);
-  };
-  
-  // Event listener for the expense form submission
-  document.querySelector('.add-form').addEventListener('submit', async (event) => {
-    event.preventDefault();
-  
-    const date = document.querySelector('#date').value.trim();
-    const category = document.querySelector('#categories').value.trim();
-    const amount = document.querySelector('#amount').value.trim();
-    const description = document.querySelector('#description').value.trim();
-  
-    if (date && category && amount) {
-      try {
-        await addExpense({ date, category, amount, description });
-        alert('Expense added!');
-      } catch (error) {
-        alert(error.message);
-      }
-    }
-  });
-  
-  // Initially fetch and render the entire expense list on page load
-  fetchAndAppendExpense();
+  }
+};
+
+// Function to append the expense to the existing list
+const appendExpenseToList = (expense) => {
+  const listContainer = document.querySelector('.list-details ul');
+
+  const listItem = document.createElement('li');
+  listItem.textContent = `${expense.date} - ${expense.category} - ${expense.amount} - ${expense.description}`;
+  listContainer.appendChild(listItem);
+};
+
+// Initially fetch and render the entire expense list on page load
+fetchAndAppendExpense();
+
