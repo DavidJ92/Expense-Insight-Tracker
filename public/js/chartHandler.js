91a586ad98fcb0eb20eb61b9ce1f8d3758cb844e
Xue <xuefeng.yangg@gmail.com>
2023-12-21 18:35:47 -0600

    added tracking for the chart

--- public/js/chartHandler.js
+++ public/js/chartHandler.js
@@ -1,64 +1,42 @@
-// User's monthly expenses data (replace this with the user's logic)
-let userMonthlyExpenses = {
-  January: 0,
-  February: 0,
-  March: 0,
-  April: 0,
-  May: 0,
-  June: 0,
-  July: 0,
-  August: 0,
-  September: 0,
-  October: 0,
-  November: 0,
-  December: 0
- };
- 
- // Function to add monthly expense for the user
- function addMonthlyExpense(month, amount) {
-  userMonthlyExpenses[month] = amount;
- }
- 
- // Function to render the monthly expense chart
- function renderExpenseChart() {
-  const currentPage = window.location.pathname; // Get the current page URL
- 
-  // Check if the current page is the home page
-  if (currentPage === '/') {
-     const ctx = document.getElementById('myChart').getContext('2d');
- 
-     // Create Chart instance
-     new Chart(ctx, {
-       type: 'line',
-       data: {
-         labels: ['January',
-          'February', 
-          'March', 
-          'April', 
-          'May', 
-          'June', 
-          'July', 
-          'August',
-           'September', 
-           'October',
-            'November', 
-            'December'],
-         datasets: [{
-           label: 'Amount Spent',
-           data: Object.values(userMonthlyExpenses),
-           borderWidth: 1
-         }]
-       },
-       options: {
-         scales: {
-           y: {
-             beginAtZero: true
-           }
-         }
-       }
-     });
-  }
- }
- 
- // Call the function to render the chart
- renderExpenseChart();
+let ctx = document.getElementById('myChart').getContext('2d');
+
+let getMonthlyExpenses = () => {
+    let monthlyExpenses = JSON.parse(localStorage.getItem('monthlyExpenses')) || {};
+    let expenses = [];
+    let labels = [];
+
+    for (let year in monthlyExpenses) {
+        for (let month in monthlyExpenses[year]) {
+            expenses.push(monthlyExpenses[year][month]);
+            labels.push(`${month}/${year}`);
+        }
+    }
+
+    return { expenses, labels };
+};
+
+let populateChart = () => {
+    let { expenses, labels } = getMonthlyExpenses();
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Monthly Expenses',
+                data: expenses,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+};
+
+// Load the chart
+populateChart();
