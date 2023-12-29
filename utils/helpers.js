74be30ff6251b50b7f85009fbf7a0829e8d91393
bxiong34 <111828461+bxiong34@users.noreply.github.com>
2023-12-29 16:41:56 -0600

    fixed container background, used prettier to write

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,9 +2,9 @@ module.exports = {
   format_date: (date) => {
     // format date as Month DD, YYYY
     return date.toLocaleDateString("en-US", {
-      month: "long", 
-      day: "numeric", 
-      year: "numeric", 
+      month: "long",
+      day: "numeric",
+      year: "numeric",
     });
   },
 };
